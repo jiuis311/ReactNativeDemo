1b991d2c7e4725327e67f912bcb3037ae5e39216
AVAVT <itsavavt@gmail.com>
2017-10-25 02:44:39 +0700

    Setup

---
+++ Practice2/generatePractice.js
@@ -0,0 +1,10 @@
+'use strict'
+require('util');
+const fs = require('fs');
+const path = require('path');
+
+function generate(numberOfTestcases){
+  return Array.from(Array(numberOfTestcases)); // Remove this line and create your own algorithm
+}
+
+module.exports = generate
